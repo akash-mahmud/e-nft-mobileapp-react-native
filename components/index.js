1ada35f82b0b7b10d10599531422aa065ae7bf85
Akash Mahmud <mahmudakash177@gmail.com>
2022-07-25 18:04:09 +0600

    add home screen and Details screen

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+
+export  {
+    FocusedStatusBar,
+    HomeHeader,
+    NFTCard
+}
